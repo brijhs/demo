7e8a93b80343bcfd8ba88724ff92833ceb27e791
brijhs <brijhs@gmail.com>
2023-02-06 10:21:51 -0500

    add dog functionality

--- animal.py
+++ animal.py
@@ -4,8 +4,14 @@ def default():
 	name=input("what is your name? ")
 	print('Hello', name)
 
+def dog(): 
+	print("Woof")
+
 def main():
-	default()
+	if sys.argv[1] == 'dog':
+		dog()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
